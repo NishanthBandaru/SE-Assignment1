cf234bae12d1aa55a0bc19062c0b4b5b4de93878
NishanthBandaru <144172747+NishanthBandaru@users.noreply.github.com>
2024-09-21 23:57:18 -0500

    Add files via upload

---
+++ SE Assignment1.py
@@ -0,0 +1,79 @@
+# Fermat's Last Theorem Near Misses
+# File Name: SE Assignment1.py
+# External files: None
+# Programmers: Nishanth Bandaru,Sai Nikhil Mamidala
+# Email : nishanthbandaru@lewisu.edu
+# Course: Software Engineering, CPSC 60500, Section 01
+# Date: 09/08/2024
+# Description: This program searches for "near misses" of the form (x, y, z, n, k) in the formula x^n + y^n = z^n,
+# where x, y, z, n, k are positive integers, and outputs the smallest relative miss found.
+
+import math
+
+# finds the near misses for the given inputs "n" and "k".
+def find_near_miss(n, k):
+    small_miss = float('inf')
+    #holding the values of x,y,z,miss for small possible miss found
+    best_x = best_y = best_z = 0 
+    actual_miss = 0 
+    count=0 # counting the possible number of miss values
+    for x in range(10, k + 1):
+        for y in range(10, k + 1):
+            xn_yn = x**n + y**n
+            #Finding the value of z
+            z = int(math.pow(xn_yn, 1/n))
+            zn = z**n
+            zn_plus_1 = (z + 1)**n
+            
+            #caliculating the misses for z^n and (z+1)^n with x^n + y^n
+            miss1 = abs(xn_yn - zn)
+            miss2 = abs(xn_yn - zn_plus_1)
+            miss = min(miss1, miss2)
+            relative_miss = miss / xn_yn
+            
+            #Finding the new small miss
+            if relative_miss < small_miss:
+                count=count+1
+                small_miss = relative_miss
+                best_x, best_y, best_z = x, y, z if miss1 < miss2 else z + 1
+                actual_miss = miss
+                print("\n===MINIMUM MISS OBSERVED===")
+                print(f"x = {x}, y = {y}, z = {best_z}\nactual miss = {actual_miss}\nrelative miss = {small_miss:.10f}")
+
+    print(f"\n#### SMALLEST RELATIVE MISS AMONG {count} OTHER MISSESS ####")
+    print(f"x = {best_x}, y = {best_y}, z = {best_z}\nactual miss = {actual_miss}\nrelative miss = {small_miss:.10f}")
+
+#Prompts the user for n and k, validates them, and calls find_near_misses.
+def main():
+    #User input n,k
+    while True:
+        try:
+            n = int(input("Enter the power n (3 <= n < 12): "))
+            if 3 <= n < 12:
+                break
+            else:
+                print("Invalid input. ensure 3 <= n < 12.")
+        except ValueError:
+            print("Invalid input. enter an integer.")
+
+    while True:
+        try:
+            k = int(input("Enter the limit k (k > 10): "))
+            if k > 10:
+                break
+            else:
+                print("Invalid input. ensure k > 10.")
+        except ValueError:
+            print("Invalid input. enter an integer.")
+
+    find_near_miss(n, k)
+
+if __name__ == "__main__":
+    while True:
+        main()
+        # searching for other near misses based on user input
+        user=input("\n\ndo you want to explore Smallest misses for other values of n,k ?\ntype yes/no \nyour answer : ")
+        if user=="yes" or user=="YES" or user=="Yes":
+            continue
+        else:
+            break
